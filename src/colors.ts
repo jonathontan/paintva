3861c74f678978254d8c1cbb59ee0396f4475a09
Jonathon Tan <jonathon.tan@hpe.com>
2025-11-05 19:23:42 +0800

    feat: add colors scheme

---
+++ src/colors.ts
@@ -0,0 +1,15 @@
+const white = '#ffffff';
+const black = '#000000';
+const teal = '#016B61';
+const green ='#70B2B2';
+const lblue = '#9ECFD4';
+const dwhite = '#E5E9C5';
+
+export default {
+  white,
+  black,
+  teal,
+  green,
+  lblue,
+  dwhite
+}

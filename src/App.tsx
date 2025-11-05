64c4ce67f0b5806ba022805540bcefa7cd608235
Jonathon Tan <jonathon.tan@hpe.com>
2025-11-05 22:46:24 +0800

    refactor: add controlpanel component into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Canvas from "./components/canvas/Canvas";
+import ControlPanel from "./components/controlpanel/ControlPanel";
 import Elementbar from "./components/elementbar/Elementbar";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div className="container">
       <Elementbar />
       <Canvas />
+      <ControlPanel />
     </div>
   )
 }

5b9a6622269efcbb8ef59cb6df3572054bd8b6b9
Jonathon Tan <jonathon.tan@hpe.com>
2025-11-07 01:34:46 +0800

    feat: add shape width and height slider

--- src/components/controlpanel/ControlPanel.tsx
+++ src/components/controlpanel/ControlPanel.tsx
@@ -13,10 +13,20 @@ interface Props {
   isDrawing: MutableRefObject<boolean>;
 }
 
+type ShapeActionType = 'shapeWidth' | 'shapeHeight' | 'none'
+
 const ControlPanel = observer(({ isDrawing }: Props) => {
-  const { selectedElement, selectedShape, selectedBrush, brushStrokeWidth, setShape, setBrush, setBrushStrokeWidth } = elementStore;
+  const {
+    selectedElement,
+    selectedShape, shapeWidth, shapeHeight, setShape, setShapeWidth, setShapeHeight,
+    selectedBrush, brushStrokeWidth, setBrush, setBrushStrokeWidth
+  } = elementStore;
   const [position, setPosition] = useState<{ x: number, y: number }>({ x: 20, y: 20 })
   const [showStrokeSlider, setShowStrokeSlider] = useState<boolean>(false);
+  const [selectedShapeAction, setSelectedShapeAction] = useState<ShapeActionType>('none')
+  const [showWidthSlider, setShowWidthSlider] = useState<boolean>(false);
+  const [showHeightSlider, setShowHeightSlider] = useState<boolean>(false);
+  const height = selectedElement === 'brush' ? 150 : selectedElement === 'shape' ? 335 : 100
 
   const handleShapeToggle = (type: ShapeType) => {
     if (selectedShape === type)
@@ -35,9 +45,34 @@ const ControlPanel = observer(({ isDrawing }: Props) => {
     }
   };
 
-  const handleSliderChange = (_event: Event, newValue: number | number[]) => {
+  const handleShapeActionToggle = (type: ShapeActionType) => {
+    console.log(selectedShapeAction, type)
+    
+    if (selectedShapeAction === type) {
+      setSelectedShapeAction('none');
+      setShowWidthSlider(false);
+      setShowHeightSlider(false);
+    } else {
+      setSelectedShapeAction(type);
+      setShowWidthSlider(type === 'shapeWidth');
+      setShowHeightSlider(type === 'shapeHeight');
+    }
+  };
+
+  const handleSliderChange = (_e: Event, type: string, newValue: number | number[]) => {
     const value = Array.isArray(newValue) ? newValue[0] : newValue
-    setBrushStrokeWidth(value);
+
+    switch (type) {
+      case 'brushStrokeWidth':
+        setBrushStrokeWidth(value)
+        break;
+      case 'shapeWidth':
+        setShapeWidth(value)
+        break;
+      case 'shapeHeight':
+        setShapeHeight(value)
+        break;
+    };
   };
 
   return (
@@ -55,8 +90,7 @@ const ControlPanel = observer(({ isDrawing }: Props) => {
       >
         <Rect
           width={100}
-          height={selectedElement === 'brush' ?
-            150 : selectedElement === 'shape' ? 220 : 100}
+          height={height}
           fill={colors.lblue}
           stroke={colors.black}
           strokeWidth={5}
@@ -87,6 +121,78 @@ const ControlPanel = observer(({ isDrawing }: Props) => {
                 <Icon icon={icon} fontSize={50} />
               </IconButton>
             ))}
+          {selectedElement === 'shape' && (
+            <>
+              <IconButton sx={{
+                position: 'absolute',
+                top: 205,
+                left: 20,
+                zIndex: 11,
+                color: selectedShapeAction === 'shapeWidth' ? colors.dwhite : colors.black
+              }}
+                onClick={() => handleShapeActionToggle('shapeWidth')}
+              >
+                <Icon icon="carbon:fit-to-width" fontSize={40} />
+              </IconButton>
+              {showWidthSlider && (
+                <Slider
+                  aria-label="Shape Width"
+                  orientation="vertical"
+                  valueLabelDisplay="auto"
+                  value={shapeWidth}
+                  onChange={(e, value) => handleSliderChange(e, 'shapeWidth', value)}
+                  min={1}
+                  max={window.innerWidth - 10}
+                  marks={false}
+                  sx={{
+                    color: colors.teal,
+                    position: 'absolute',
+                    top: 0,
+                    left: 100,
+                    width: 10,
+                    height: height - 5,
+                    zIndex: 12
+                  }}
+                />
+              )}
+            </>
+          )}
+          {selectedElement === 'shape' && (
+            <>
+              <IconButton sx={{
+                position: 'absolute',
+                top: 265,
+                left: 20,
+                zIndex: 11,
+                color: selectedShapeAction === 'shapeHeight' ? colors.dwhite : colors.black
+              }}
+                onClick={() => handleShapeActionToggle('shapeHeight')}
+              >
+                <Icon icon="carbon:fit-to-height" fontSize={40} />
+              </IconButton>
+              {showHeightSlider && (
+                <Slider
+                  aria-label="Shape Height"
+                  orientation="vertical"
+                  valueLabelDisplay="auto"
+                  value={shapeHeight}
+                  onChange={(e, value) => handleSliderChange(e, 'shapeHeight', value)}
+                  min={1}
+                  max={window.innerHeight - 10}
+                  marks={false}
+                  sx={{
+                    color: colors.teal,
+                    position: 'absolute',
+                    top: 0,
+                    left: 100,
+                    width: 10,
+                    height: height - 5,
+                    zIndex: 12
+                  }}
+                />
+              )}
+            </>
+          )}
           {selectedElement === 'brush' && (
             <>
               <IconButton sx={{
@@ -106,17 +212,17 @@ const ControlPanel = observer(({ isDrawing }: Props) => {
                   valueLabelDisplay="auto"
                   value={brushStrokeWidth}
                   defaultValue={0}
-                  onChange={handleSliderChange}
+                  onChange={(e, value) => handleSliderChange(e, 'brushStrokeWidth', value)}
                   min={1}
                   max={50}
                   marks={false}
                   sx={{
                     color: colors.teal,
                     position: 'absolute',
-                    top: 10,
+                    top: 0,
                     left: 100,
                     width: 10,
-                    height: 200,
+                    height: height - 5,
                     zIndex: 12
                   }}
                 />
